f5e3113c04d69959f488f766336a55ce5f26a91f
Oleh Brusentsov <oleh.brusentsov@gmail.com>
2016-04-09 20:54:57 +0300

    Добавил изображения не закомиченные через шторм в предыдущий раз + отредактировал текст и добавил стиль h1

--- index.php
+++ index.php
@@ -2,19 +2,17 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Что оно такое php-academy (php-academy.kiev.ua)? Впечатления vs Разочарования от курса "PHP интенсив 2 мес.".</title>
+    <title>Что оно такое - php-academy (php-academy.kiev.ua)? Впечатления и разочарования от посещения курса: PHP интенсив 2 мес.</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="img/favicon.png">
 </head>
 <body>
 <header>
     <div class="header-block"><a href="http://php-academy.com"><img src="img/logo.png"/></a>
-        <p>Обучение в школе програмирование <a href="http://php-academy.kiev.ua" target="_blank">php-academy.kiev.ua</a>. Впечатления, отзывы по курсу - “PHP интенсив 2
-            мес.”.
-        </p>
+        <p>Обучение в школе програмирование <a href="http://php-academy.kiev.ua" target="_blank">php-academy.kiev.ua</a>. Впечатления и разочарования от посещения курса: <b>PHP интенсив 2 мес.</b></p>
     </div>
 </header>
-
+<div><h1>О проекте: </h1></div>
 <footer></footer>
 </body>
 </html>
