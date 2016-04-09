bb042c202a7742463ad976b94f9f6039b748d596
Oleh Brusentsov <oleh.brusentsov@gmail.com>
2016-04-09 20:22:24 +0300

    Изменил шапку, добавил лого + описание, фон страницы и т.д.

---
+++ index.php
@@ -0,0 +1,20 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Что оно такое php-academy (php-academy.kiev.ua)? Впечатления vs Разочарования от курса "PHP интенсив 2 мес.".</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/favicon.png">
+</head>
+<body>
+<header>
+    <div class="header-block"><a href="http://php-academy.com"><img src="img/logo.png"/></a>
+        <p>Обучение в школе програмирование <a href="http://php-academy.kiev.ua" target="_blank">php-academy.kiev.ua</a>. Впечатления, отзывы по курсу - “PHP интенсив 2
+            мес.”.
+        </p>
+    </div>
+</header>
+
+<footer></footer>
+</body>
+</html>
