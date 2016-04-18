2b677886d116abb828d9aec7b613f1c607f0688f
Oleh Brusentsov <oleh.brusentsov@gmail.com>
2016-04-18 22:47:46 +0300

    lesson 3 other

---
+++ lessons/03/dz_oleg/arrays_loops_tasks/23.php
@@ -0,0 +1,33 @@
+﻿<?php
+// 23. Вам нужно разработать программу, которая считала бы сумму цифр числа введенного
+// пользователем. Например: есть число 123, то программа должна вычислить сумму цифр 1, 2, 3, т. е. 6.
+
+if (is_numeric($_POST['number'])) {
+    $res = NULL;
+    foreach (str_split($_POST['number']) as $value) {
+        $res += $value;
+    }
+    echo "Сумма чисел введенного числа: " . $res;
+}
+else {
+    if (!$_POST['number'] == NULL) {
+        echo "Можно вводить только цифры! Пожалуйста повторите попытку!";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Введите число:</title>
+</head>
+<body>
+<form method="POST">
+    <label for="number">Пожалуйста введите число:</label>
+    <input type="text" name = "number" id="number">
+
+    <input type="submit" value="Отправить">
+</form>
+</body>
+</html>
---
+++ lessons/03/dz_oleg/arrays_loops_tasks/24.php
@@ -0,0 +1,34 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title>Введите число:</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+
+<?php
+// 24. Вам нужно разработать программу, которая считала бы количество вхождений
+// какой­нибуть выбранной вами цифры в выбранном вами числе. Например: цифра 5 в числе 442158755745 встречается 4 раза.
+
+if (is_numeric($_POST['number']) && is_numeric($_POST['search'])) {
+    $res = substr_count($_POST['number'], $_POST['search']);
+    echo "Число {$_POST['search']} , встречается {$res} раз(а) в строке {$_POST['number']}!";
+}
+else {
+    if (!$_POST['number'] == NULL || !$_POST['search'] == NULL) {
+        echo "Можно вводить только цифры! Пожалуйста повторите попытку! <hr>";
+    }
+}
+?>
+
+<form method="POST">
+    <label for="number">Число в котором искать:</label>
+    <input type="text" name = "number" id="number">
+
+    <label for="search">Число для поиска:</label>
+    <input type="text" name = "search" id="search">
+
+    <input type="submit" value="Отправить">
+</form>
+</body>
+</html>
